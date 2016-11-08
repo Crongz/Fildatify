3fe26d1ff91a1cc467a46864bfe2a30aa9bc272b
James Lee <leejamesws@gmail.com>
2016-11-08 17:53:24 -0600

    db

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://filmdatify:veryInsecure74@fa16-cs411-06.cs.illinois.edu/filmdatify"
+db = SQLAlchemy(app)
+
+result = db.engine.execute("""SELECT * FROM "public"."users" """)
+print (result)
