45a2401c8f6864a09573c5f7509e9727e649a7ab
James Lee <leejamesws@gmail.com>
2016-11-08 16:01:24 -0600

    changed config and requirements.txt

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ from app.view import view
 app.register_blueprint(view)
 
 if __name__ == "__main__":
-	app.run(host='fa16-cs411-06.cs.illinois.edu', port=5000)
+	app.run(host='localhost', port=5000)
